ef60be251a11d85f6b52185bee5a4134dafa29f2
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-07-20 12:56:45 +1000

    initial commit

---
+++ data_sources.py
@@ -0,0 +1,66 @@
+import os
+
+data = 'data'
+
+# greenhouse_dataset
+
+# m5_forecasting_accuracy
+
+# nifty_50_stock_market_data_2000_2020
+
+# norway_bicycle
+
+# partial visuelle
+
+# telco customer churn
+
+# ts_data_1
+ambient_temperature_system_failure = os.path.join(data, 'ts_data_1', 'ambient_temperature_system_failure.csv')
+arma_series1 = os.path.join(data, 'ts_data_1', 'arma_series1.csv')
+arma_series2 = os.path.join(data, 'ts_data_1', 'arma_series2.csv')
+arma_series3 = os.path.join(data, 'ts_data_1', 'arma_series3.csv')
+avocado = os.path.join(data, 'ts_data_1', 'avocado.csv')
+daily_minimum_temperatures_in_me = os.path.join(data, 'ts_data_1', 'daily-minimum-temperatures-in-me.csv')
+dataset_temperature = os.path.join(data, 'ts_data_1', 'dataset_temperature.csv')
+diabetic = os.path.join(data, 'ts_data_1', 'diabetic.csv')
+energy_daily = os.path.join(data, 'ts_data_1', 'energy_daily.csv')
+ex1_wiki_visits = os.path.join(data, 'ts_data_1', 'ex1_wiki_visits.csv')
+example_air_passengers = os.path.join(data, 'ts_data_1', 'example_air_passengers.csv')
+example_retail_sales = os.path.join(data, 'ts_data_1', 'example_retail_sales.csv')
+example_wp_log_peyton_manning = os.path.join(data, 'ts_data_1', 'example_wp_log_peyton_manning.csv')
+example_wp_log_R_outliers1 = os.path.join(data, 'ts_data_1', 'example_wp_log_R_outliers1.csv')
+example_wp_log_R_outliers2 = os.path.join(data, 'ts_data_1', 'example_wp_log_R_outliers2.csv')
+exp1 = os.path.join(data, 'ts_data_1', 'exp1.csv')
+exp2 = os.path.join(data, 'ts_data_1', 'exp2.csv')
+exp3 = os.path.join(data, 'ts_data_1', 'exp3.csv')
+Fremont_Bridge = os.path.join(data, 'ts_data_1', 'Fremont_Bridge.csv')
+google_chg = os.path.join(data, 'ts_data_1', 'google_chg.csv')
+irish_electricity1 = os.path.join(data, 'ts_data_1', 'irish_electricity.csv')
+irish_electricity2 = os.path.join(data, 'ts_data_1', 'irish_electricity.xlsx')
+medium_posts = os.path.join(data, 'ts_data_1', 'medium_posts.csv')
+outdoor_temperature_UTC = os.path.join(data, 'ts_data_1', 'outdoor-temperature-12UTC.csv')
+passengers = os.path.join(data, 'ts_data_1', 'passengers.csv')
+passengers_test = os.path.join(data, 'ts_data_1', 'passengers_test.csv')
+passengers_train = os.path.join(data, 'ts_data_1', 'passengers_train.csv')
+quarterly_gdp_change = os.path.join(data, 'ts_data_1', 'quarterly_gdp_change.csv')
+rossman_sales = os.path.join(data, 'ts_data_1', 'rossman_sales.csv')
+Sample_Superstore = os.path.join(data, 'ts_data_1', 'Sample - Superstore.xls')
+savings_change = os.path.join(data, 'ts_data_1', 'savings_change.csv')
+sunspots = os.path.join(data, 'ts_data_1', 'sunspots.csv')
+tesla_prices_5y = os.path.join(data, 'ts_data_1', 'tesla_prices_5y.csv')
+us_covid = os.path.join(data, 'ts_data_1', 'us_covid.csv')
+us_energy = os.path.join(data, 'ts_data_1', 'us_energy.csv')
+wiki_log_R = os.path.join(data, 'ts_data_1', 'wiki_log_R.csv')
+WHO_full_data2003 = os.path.join(data, 'who_cases', 'WHO_full_data2003.csv')
+pjm_hourly_est = os.path.join(data, 'hourly_energy_consumption', 'pjm_hourly_est.csv')
+bikerides_day = os.path.join(data, 'norway_bicycle', 'bikerides_day.csv')
+Region_CAL = os.path.join(data, 'us_energy_consumption', 'Region_CAL.xlsx')
+
+# US energy consumption
+
+# ventilator pressure prediction
+
+# WHO cases
+
+sales_training_validation = os.path.join(data, 'm5_forecast_accuracy', 'sales_training_validation')
+sales_train_evaluation = os.path.join(data, 'm5_forecast_accuracy', 'sell_prices.csv')
